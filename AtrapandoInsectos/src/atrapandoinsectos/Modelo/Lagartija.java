da2abeeb5625f643472eb63fe1dddacc13accd30
jesfacap <jesfacap@espol.edu.ec>
2019-08-09 18:49:22 -0500

    posicion inicial de la lagartija

--- AtrapandoInsectos/src/atrapandoinsectos/Modelo/Lagartija.java
+++ AtrapandoInsectos/src/atrapandoinsectos/Modelo/Lagartija.java
@@ -12,17 +12,51 @@ import javafx.scene.image.ImageView;
  *
  * @author pc
  */
-public class Lagartija extends Insecto{
+package atrapandoinsectos.Modelo;
+
+import java.util.Random;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author pc
+ */
+public class Lagartija extends Insecto {
 
     public Lagartija(int puntos) {
         super(puntos);
-        Image img=new Image(getClass().getResourceAsStream("/Recursos/Imagenes/lagartija.png"),75,75,true,true);
+        Image img = new Image(getClass().getResourceAsStream("/Recursos/Imagenes/Lagartija-g.gif"), 175, 175, true, true);
         imagen = new ImageView(img);
+        imagen.relocate(100, 0);
+        posicion();
+        //lagartija.relocate(50, 0);
     }
 
     @Override
     public void run() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    /*
+    meto para la posicion inicial de la lagartija
+     */
+    public void posicion() {
+        Random r = new Random();
+        int valorDado = r.nextInt(4);
+        if (valorDado == 0) {
+            imagen.relocate(80, 0);
+        }
+        if (valorDado == 1) {
+            imagen.relocate(950, 0);
+        }
+        if (valorDado == 2) {
+            imagen.relocate(80, 500);
+        }
+        if (valorDado == 3) {
+            imagen.relocate(950, 500);
+
+        }
+    }
+
 }
