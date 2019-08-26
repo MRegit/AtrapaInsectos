4ecdb37938f851d768237cc8fcf4bdedf145a786
jesfacap <jesfacap@espol.edu.ec>
2019-08-26 01:08:56 -0500

    colisiones

--- AtrapandoInsectos/src/atrapandoinsectos/Interfaz/Nivel2.java
+++ AtrapandoInsectos/src/atrapandoinsectos/Interfaz/Nivel2.java
@@ -2,11 +2,13 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */package atrapandoinsectos.Interfaz;
+ */
+package atrapandoinsectos.Interfaz;
 
-import static atrapandoinsectos.Interfaz.PantallaMenu.jugador;
 import atrapandoinsectos.Modelo.Araña;
+import atrapandoinsectos.Modelo.Hormiga;
 import atrapandoinsectos.Modelo.Mosca;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,25 +32,23 @@ import javafx.stage.Stage;
  */
 public class Nivel2 extends Nivel1 {
 
-    
     private ImageView roca1;
     private ImageView roca2;
     private ImageView roca3;
     private ImageView hoja1;
     private ImageView hoja2;
     private ImageView hoja3;
- 
+    private static List<ImageView> rocas = new ArrayList<>(); //lista de las rocas
+    private static List<ImageView> hojas = new ArrayList<>(); //lista de las hojas
 
-    
-    public Nivel2(){
+    public Nivel2() {
         super();
-        
 
     }
 
     @Override
     public void inicializar() {
-        
+
         super.inicializar();
         roca1 = new ImageView(new Image("/Recursos/Imagenes/Roca.png"));
         roca2 = new ImageView(new Image("/Recursos/Imagenes/Roca.png"));
@@ -66,7 +66,7 @@ public class Nivel2 extends Nivel1 {
         hoja2 = new ImageView(new Image("/Recursos/Imagenes/Hoja2.png"));
         hoja3 = new ImageView(new Image("/Recursos/Imagenes/Hoja2.png"));
         hoja1.relocate(200, 120);
-        hoja2.relocate(550, 350);
+        hoja2.relocate(550, 450);
         hoja3.relocate(870, 200);
         hoja1.setFitHeight(100);
         hoja1.setFitWidth(100);
@@ -74,20 +74,42 @@ public class Nivel2 extends Nivel1 {
         hoja2.setFitWidth(100);
         hoja3.setFitHeight(100);
         hoja3.setFitWidth(100);
-        puntosGanar=150;
-        jugador.setNivelAlcanzado(2);
+        rocas.add(roca1);
+        rocas.add(roca2);
+        rocas.add(roca3);
+        hojas.add(hoja1);
+        hojas.add(hoja2);
+        hojas.add(hoja3);
+        
+        puntosGanar = 150;
 
     }
 
     @Override
     public void organizar() {
 
-       super.organizar();
-       gamePane.getChildren().addAll(roca1, roca2, roca3, hoja1, hoja2, hoja3);
-   
-       
+        super.organizar();
+        gamePane.getChildren().addAll(roca1, roca2, roca3, hoja1, hoja2, hoja3);
+
     }
 
+    public static boolean ColisionRocas() {
 
- 
+        for (ImageView r : rocas) {
+            if (Nivel1.isCollision(r, PantallaMenu.jugador.getObjeto())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean ColisionHojas() {
+        for (ImageView h : hojas) {
+            if (Nivel1.isCollision(h, PantallaMenu.jugador.getObjeto())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
